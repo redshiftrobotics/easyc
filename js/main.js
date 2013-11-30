c4511a47548529838310290f15a90920f1607562
Alex Jordan <alexander3223098@gmail.com>
2013-11-30 15:18:10 -0800

    fix #pragma config() issues

--- js/main.js
+++ js/main.js
@@ -5,8 +5,19 @@ var BlocksAdded = 0;
 var Context;
 var Canvas;
 
-var libraryRequests = ["drivers/common", "I2C", "motors", "servos"];
-var libraryText = ["drivers/common", "I2C", "motors", "servos"];
+var libraryRequests = ["drivers/common", "I2C", "Motors", "Servos"];
+var libraryText = ["drivers/common", "I2C", "Motors", "Servos"];
+
+// TODO: don't use rawgithub.com
+// we have to use rawgithub.com instead of raw.github.com because GitHub sends a MIME of text/plain and XMLHttpRequest only accepts text/html, text/xml, etc.
+const var library_base_url = "https://rawgithub.com/saasrobotics/Robotics2013-14/master/libraries/";
+
+const var pragmaConfig = "
+#pragma config(Sensor, S1,     ,               sensorI2CCustom)
+#pragma config(Sensor, S2,     ,               sensorI2CCustom)
+#pragma config(Sensor, S3,     ,               sensorI2CCustom)
+#pragma config(Sensor, S4,     ,               sensorI2CCustom)
+"
 
 function RefreshPage()
 {
@@ -32,7 +43,7 @@ function recieveLibrary(name)
 		console.log("loaded all libraries");
 		
 		// instantiate the data into JavaScript
-		CodeAddition = libraryText["drivers/common"] + libraryText["I2C"] + libraryText["motors"] + libraryText["servos"];
+		CodeAddition = pragmaConfig + libraryText["drivers/common"] + libraryText["I2C"] + libraryText["Motors"] + libraryText["Servos"];
 		
 		// get rid of the infobar and enable the app
 		$("#loadingAlert").remove();
